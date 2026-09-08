54947fe192c57f4bc4c2f6d9d6d8e86c87f2acde
nk6341350-cloud <nk6341350@gmail.com>
2026-09-08 17:16:19 +0430

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='ishaqzada-v11';
-const ASSETS=['./','index.html','styles.css?v=11','app.js?v=11','manifest.webmanifest','assets/logo.jpeg'];
+const CACHE='ishaqzada-v12';
+const ASSETS=['./','index.html','styles.css?v=12','app.js?v=12','manifest.webmanifest','assets/logo.jpeg'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>{
